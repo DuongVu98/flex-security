07cb9b9b24a9e173550297c8a9770606e8d9ad10
Vu Tung Duong <duong.vu@bigin.vn>
2019-12-16 01:03:42 +0700

    last commit

--- src/test/java/com/hcmiuiot/MicroSec/UserReopsitoryTest.java
+++ src/test/java/com/hcmiuiot/MicroSec/UserReopsitoryTest.java
@@ -2,7 +2,9 @@ package com.hcmiuiot.MicroSec;
 
 import com.hcmiuiot.MicroSec.models.User;
 import com.hcmiuiot.MicroSec.repositories.UserRepository;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,8 +20,12 @@ public class UserReopsitoryTest {
 
     @Test
     @DirtiesContext
+    @DisplayName("User repository with right username")
     void updateUsername(){
         User user = userRepository.findByUsername("tom@gmail.com");
         userRepository.updateUser(user.getId(), "tom123@gmail.com");
+
+        User user1 = userRepository.findByUsername("tom123@gmailc.om");
+        Assert.assertNotNull(user);
     }
 }
